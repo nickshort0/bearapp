a4766fe9b5937aefd204c5d18c63611f8c0b182e
nickshort0 <therealnickshort@gmail.com>
2022-06-21 19:19:09 -0700

    added armor service interface

--- src/main/java/com/casestudy/bearapp/service/ArmorService.java
+++ src/main/java/com/casestudy/bearapp/service/ArmorService.java
@@ -1,4 +1,12 @@
 package com.casestudy.bearapp.service;
 
+import com.casestudy.bearapp.models.Armor;
+
+import java.util.List;
+
 public interface ArmorService {
+    List<Armor> getAllArmors();
+    void saveArmor(Armor armor);
+    Armor getArmorById(long id);
+    void deleteArmorById(long id);
 }
--- src/main/java/com/casestudy/bearapp/service/ArmorServiceImp.java
+++ src/main/java/com/casestudy/bearapp/service/ArmorServiceImp.java
@@ -1,4 +1,29 @@
 package com.casestudy.bearapp.service;
 
-public class ArmorServiceImp {
+import com.casestudy.bearapp.models.Armor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArmorServiceImp implements ArmorService{
+    @Override
+    public List<Armor> getAllArmors() {
+        return null;
+    }
+
+    @Override
+    public void saveArmor(Armor armor) {
+
+    }
+
+    @Override
+    public Armor getArmorById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteArmorById(long id) {
+
+    }
 }
