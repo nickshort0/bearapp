abad1a91b5c44c409316e9b88b5e280184e71690
nickshort0 <therealnickshort@gmail.com>
2022-06-28 11:41:30 -0700

    pagination implemented

--- src/main/java/com/casestudy/bearapp/BearappApplication.java
+++ src/main/java/com/casestudy/bearapp/BearappApplication.java
@@ -16,5 +16,23 @@ public class BearappApplication {
 		SpringApplication.run(BearappApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(BearRepository repo){
+		return (args -> {
+			insertJavaAdvocates(repo);
+		});
+	}
+
+	private void insertJavaAdvocates(BearRepository repo){
+		repo.save(new Bear("Bear1", 1,1));
+		repo.save(new Bear("Bear2", 2,2));
+		repo.save(new Bear("Bear3", 3,3));
+		repo.save(new Bear("Bear4", 4,4));
+		repo.save(new Bear("Bear5", 5,5));
+		repo.save(new Bear("Bear6", 6,6));
+		repo.save(new Bear("Bear7", 70,100));
+
+
+	}
 
 }
