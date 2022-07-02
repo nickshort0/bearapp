2c9dd5f86aa66fe0839ff196ed9e608cbd75bc61
nickshort0 <therealnickshort@gmail.com>
2022-07-02 11:25:47 -0700

    security dependencies

--- src/main/java/com/casestudy/bearapp/models/User.java
+++ src/main/java/com/casestudy/bearapp/models/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -29,6 +30,7 @@ public class User {
     @Setter(AccessLevel.NONE) @NonNull
     String password;
 
+    String role;
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
     @JoinTable(name = "user_bears",
     joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"))
@@ -42,4 +44,16 @@ public class User {
         bears.remove(bear);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, password);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return id == user.id && email.equals(user.email) && username.equals(user.username) && password.equals(user.password);
+    }
 }
