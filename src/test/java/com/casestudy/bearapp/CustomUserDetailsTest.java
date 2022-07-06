c1ee767fdd834ce289386149dae9cb19fa4d0ea0
nickshort0 <therealnickshort@gmail.com>
2022-07-05 21:40:39 -0700

    parameterized test

--- src/test/java/com/casestudy/bearapp/CustomUserDetailsTest.java
+++ src/test/java/com/casestudy/bearapp/CustomUserDetailsTest.java
@@ -1,16 +1,22 @@
 package com.casestudy.bearapp;
 
 
+import com.casestudy.bearapp.models.Bear;
 import com.casestudy.bearapp.models.User;
 import com.casestudy.bearapp.service.CustomUserDetailsService;
 import com.casestudy.bearapp.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,5 +41,7 @@ class CustomUserDetailsServiceTest {
         userService.saveUser(user);
         assertThat(customUserDetailsService.loadUserByUsername("Test User")).isNotNull();
     }
+
+
 
 }
--- src/test/java/com/casestudy/bearapp/UserServiceTest.java
+++ src/test/java/com/casestudy/bearapp/UserServiceTest.java
@@ -4,28 +4,48 @@ package com.casestudy.bearapp;
 import com.casestudy.bearapp.models.User;
 import com.casestudy.bearapp.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Slf4j
 class UserServiceTest {
     @Autowired
     UserService userService;
 
+    @Order(1)
     @Test
     void saveUser(){
         User User = new User("Test User", "email@email.com", "password");
         userService.saveUser(User);
         assertThat(userService.getUserById(1)).isEqualTo(User);
+    }
+
+    @Order(2)
+    @ParameterizedTest
+    @ValueSource(strings = {"Test User", "Test User 2"})
+    void checkUsername(String usernames){
+        User user = new User("Test User 2", "email@email.com", "password");
+        userService.saveUser(user);
+        List<User> users = userService.getAllUsers();
+        assertThat(users).extracting(User::getUsername).contains(usernames);
     }
 
 }
